afb24daecc45d98cba2f67b43587e833f8efc62d
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2018-05-26 23:43:43 +0700

    edit __init__.py for production

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,25 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
 from config import app_config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
+        
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')

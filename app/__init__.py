bab87f12129a39497ab132ce2e3355697fdae629
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2018-05-26 23:57:37 +0700

    edit __init__.py for config env var

--- app/__init__.py
+++ app/__init__.py
@@ -22,10 +22,6 @@ def create_app(config_name):
         app = Flask(__name__, instance_relative_config=True)
         app.config.from_object(app_config[config_name])
         app.config.from_pyfile('config.py')
-        
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
 
     Bootstrap(app)
     db.init_app(app)

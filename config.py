99462a7679f215589c06dafcb1d471d3fd12fc1d
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2018-05-26 22:51:39 +0700

    part three done exclude deploy

--- config.py
+++ config.py
@@ -2,24 +2,32 @@ class Config(object):
     """
     Common configurations
     """
-    pass
+
+    DEBUG = True
 
 class DevelopmentConfig(Config):
     """
     Development configurations
     """
 
-    DEBUG = True
     SQLALCHEMY_ECHO = True
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
+
     DEBUG = False
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
 
 app_config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig
+    'production': ProductionConfig,
+    'testing': TestingConfig
 }
-
